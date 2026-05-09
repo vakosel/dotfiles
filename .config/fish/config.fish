89ff570767e07f560337f4dc9b4e14544be399e2
vakosel <vakosel@yahoo.gr>
2026-05-09 19:55:50 +0300

    alacritty alliases code

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -74,7 +74,3 @@ end
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /home/vakosel/.lmstudio/bin
-# End of LM Studio CLI section
